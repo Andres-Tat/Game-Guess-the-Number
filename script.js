7c386db774ccecc7ef4f3b457444a59b8e6583c1
Andres Tatarnikov <119009469+Andres-Tat@users.noreply.github.com>
2024-03-28 19:52:20 +0300

    push first

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+///////////////////////////////////////
+
+/* 
+ЗАДАЧА: 
+Создайте игру в угадай число.
+
+
+Удачи)
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+let highScore = 0;
+
+
