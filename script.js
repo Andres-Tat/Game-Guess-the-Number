8c095ebff7a539f47aa9676633b34400f91c40b1
Andres Tatarnikov <119009469+Andres-Tat@users.noreply.github.com>
2024-03-28 20:02:55 +0300

    add function

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-"use strict";
-
-///////////////////////////////////////
-
-/* 
-ЗАДАЧА: 
-Создайте игру в угадай число.
-
-
-Удачи)
-*/
+'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
 let highScore = 0;
 
+// Создаю функцию, которая будет сокращать код в дальнейшем
+const displayMessage = function (message) {
+  document.querySelector(message).textContent = message;
+};
 
+document.querySelector('.check');
